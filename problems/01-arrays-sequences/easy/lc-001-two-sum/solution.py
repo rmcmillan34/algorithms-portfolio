f057882d16965c796ea392a2607c20920c9b4295
Ryan McMillan <ryan.j.mcmillan34@gmail.com>
2026-02-28 19:44:43 +1100

    (problem): added Python3 solution LC-001

--- problems/01-arrays-sequences/easy/lc-001-two-sum/solution.py
+++ problems/01-arrays-sequences/easy/lc-001-two-sum/solution.py
@@ -1,5 +1,15 @@
 # LC-001 Two Sum
 
+from typing import Sequence
+from typing import List
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+        seen = {}
+
+        for idx, num in enumerate(nums):
+            complement = target - num
+            if not complement in seen:
+                seen[num] = idx
+            else:
+                return [idx, seen[complement]]
